b8d9f997e5ae75b89e130e4fa3979db1009e49b8
BashirHilaly <bashirkenza@gmail.com>
2024-02-13 22:33:46 -0500

    Created calculating grades files

---
+++ Lab Entering Grades/CmpSc132_Grades_Class.py
@@ -0,0 +1,19 @@
+"""
+----------------------------------------------------
+-- CMPSC_132, Spring 2024, Bashir Hilaly
+-- Lab: Entering Grades Class Version
+-- Grades File
+----------------------------------------------------
+"""
+
+class CmpS132Grades():
+
+# Private fields... I think ------
+    # Total of all grades entered
+    __total_grades = []
+    # Total of all Valid grades entered
+    __total_valid_grades = []
+    # Total of all Invalid grade entered
+    __total_invalid_grades = []
+
+    pass
