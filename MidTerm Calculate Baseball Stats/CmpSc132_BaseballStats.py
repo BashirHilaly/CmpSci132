6a6476d33b75cd0d131fa3bba044c130932757d9
BashirHilaly <bashirkenza@gmail.com>
2024-02-28 00:50:28 -0500

    Created main file and player class

--- MidTerm Calculate Baseball Stats/CmpSc132_BaseballStats.py
+++ MidTerm Calculate Baseball Stats/CmpSc132_BaseballStats.py
@@ -1,4 +1,19 @@
+class Player():
 
+    def __init__(self, playerName, stats):
+        self.playerName = playerName
+        self.rawStats = stats
+
+def removeNewLineChar(string):
+    return string.strip()
 
 if __name__ == "__main__":
-    print('lets get it')
+    
+    file = "D:/School/CMPSCI 132/MidTerm Calculate Baseball Stats/Lab_BaseballStats.txt"
+
+    # Open text file and read contents
+    lines = []
+    with open(file) as f:
+        lines = f.readlines()
+        f.close()
+        
