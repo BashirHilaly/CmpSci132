4120730de5015c6994772552882c9d713c97104e
BashirHilaly <bashirkenza@gmail.com>
2024-03-01 01:34:38 -0500

    Created the validateStats function

--- MidTerm Calculate Baseball Stats/CmpSc132_BaseballStats.py
+++ MidTerm Calculate Baseball Stats/CmpSc132_BaseballStats.py
@@ -4,6 +4,31 @@ class Player():
         self.playerName = playerName
         self.rawStats = stats
 
+# Stats dictionary
+AtBats = 0
+Hits = 1
+Double = 2
+Triples = 3
+Homeruns = 4
+Walks = 5
+
+def validateStats(stats):
+    # Return true if stats are valid
+
+    # All stats must have single digit
+    for i in stats:
+        if int(i) > 9:
+            return False
+    # number of Hits cannot be exceeded the # of At Bats.
+    if int(stats[Hits]) > int(stats[AtBats]):
+        return False
+    # Doubles, Triples, and Home runs cannot exceed # of Hits.
+    if (int(stats[Doubles]) + int(stats[Triples]) + int(stats[Homeruns])) > int(stats[Hits]):
+        return False
+
+    return True
+
+
 def removeNewLineChar(string):
     return string.replace('\n', '')
 
@@ -26,9 +51,21 @@ if __name__ == "__main__":
         if lines[i] == lines[0]:
             name = removeNewLineChar(lines[i])
         else:
-            # Check if
-            rawStats.append(removeNewLineChar(lines[i]))
-    
+            # Check if its not empty
+            if removeNewLineChar(lines[i]) != '':
+                rawStats.append(removeNewLineChar(lines[i]))
+
     print(name)
     print(rawStats)
+
+    cleanStats = []
+    # Convert list of strings into list of lists
+    for stat in rawStats:
+        cleanStats.append(stat.split())
+
+    print(cleanStats)
+    
+    # First validate statistics
+    for stat in rawStats:
+        print('\nIs stat valid?: ' + validateStats(stat))
         
