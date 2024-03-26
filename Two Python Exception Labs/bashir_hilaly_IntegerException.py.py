f3b5e63556839e724cbc58d0b29e0c8923e56997
BashirHilaly <bashirkenza@gmail.com>
2024-03-26 03:24:17 -0400

    Finished Lab 1

---
+++ Two Python Exception Labs/bashir_hilaly_IntegerException.py.py
@@ -0,0 +1,30 @@
+"""
+----------------------------------------------------
+-- CMPSC_132, Spring 2024, Bashir Hilaly
+-- LLab 1 – Detecting integer exception
+----------------------------------------------------
+"""
+
+
+def main():
+    parts = input().split()
+    name = parts[0]
+    output = []
+    while name != '-1':
+        # FIXME: The following line will throw ValueError exception.
+        # Insert try/except blocks to catch the exception.
+        try:
+            age = int(parts[1]) + 1
+            output.append(f'{name} {age}')
+        except ValueError:
+            output.append(f'{name} 0')
+        # Get next line
+        parts = input().split()
+        name = parts[0]
+
+    for name in output:
+        print(name)
+
+
+if __name__ == '__main__':
+    main()
