cf6cfd90d04ea6355fb3896f6cb639cbac4b35df
BashirHilaly <bashirkenza@gmail.com>
2024-04-27 15:26:24 -0400

    Validated rows and columns

--- Final Sodoku Validation/main.py
+++ Final Sodoku Validation/main.py
@@ -17,6 +17,46 @@ samplePuzzle = [ [ 7, 8, 9, 1, 2, 3, 4, 5, 6],
             [ 6, 7, 8, 9, 1, 2, 3, 4, 5],
             [ 9, 1, 2, 3, 4, 5, 6, 7, 8] ]
 
+def validatePuzzle(puzzle):
+    isValid = True
+    invalidMessage = ''
+
+    # Check if all rows are valid
+    for row in puzzle:
+        validNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        for number in row:
+            if number in validNumbers:
+                validNumbers.remove(number)
+        if validNumbers != []:
+            isValid = False
+            invalidMessage = 'Invalid Row!'
+        # print(validNumbers)
+
+    if not isValid:
+        print(invalidMessage)
+        return invalidMessage
+    
+    # Check if all columns are valid
+    pos = 0
+    for i in range(9):
+        validNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        column = []
+        for row in puzzle:
+            column.append(row[pos])
+        for number in column:
+            if number in validNumbers:
+                validNumbers.remove(number)
+        if validNumbers != []:
+            # Find a way to end the loop here
+            isValid = False
+            invalidMessage = 'Invalid Column'
+        pos += 1
+    
+    if not isValid:
+        print(invalidMessage)
+        return invalidMessage
+
+
 def generateRandomPuzzle():
     createdPuzzle = []
 
@@ -49,5 +89,9 @@ def printPuzzle(puzzle):
 
 if __name__ == '__main__':
     puzzle = generateRandomPuzzle()
+    #puzzle = samplePuzzle
 
     printPuzzle(puzzle)
+
+    print('\n\n')
+    validatePuzzle(puzzle)
